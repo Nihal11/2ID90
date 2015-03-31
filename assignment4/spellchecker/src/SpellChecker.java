0f92bd636050287fdab04c64c7d64ca339875123
dennisschagt <dennisschagt@gmail.com>
2015-03-31 13:18:13 +0200

    Only show output for teests with wrong answer

--- assignment4/spellchecker/src/SpellChecker.java
+++ assignment4/spellchecker/src/SpellChecker.java
@@ -28,28 +28,51 @@ public class SpellChecker {
     
     static void nonPeachTest(SpellCorrector sc) throws IOException { 
             String[] sentences = {
-//                "at the hme locations there were traces of water",
+                "at the hme locations there were traces of water",
                 "helo ladies and gentelmen",
                 "kind retards",
-//                "this assay allowed us to measure a wide variety of conditions",
-//                "this assay allowed us to measure a wide variety of conitions",
-//                "this assay allowed us to meassure a wide variety of conditions",
+                "this assay allowed us to measure a wide variety of conditions",
+                "this assay allowed us to measure a wide variety of conitions",
+                "this assay allowed us to meassure a wide variety of conditions",
                 "this assay allowed us to measure a wide vareity of conditions",
-//                "at the home locations there were traces of water",
+                "at the home locations there were traces of water",
                 "at the hme locations there were traces of water",
                 "at the hoome locations there were traces of water",
-//                "at the home locasions there were traces of water",
-//                "the development of diabetes is present in mice that carry a transgen",
-//                "the development of diabetes is present in moce that carry a transgen",
+                "at the home locasions there were traces of water",
+                "the development of diabetes is present in mice that carry a transgen",
+                "the development of diabetes is present in moce that carry a transgen",
                 "the development of idabetes is present in mice that carry a transgen",
                 "the development of diabetes us present in mice that harry a transgen",
             };
+            String[] correctSentences = {
+                "at the home locations there were traces of water",
+                "hello ladies and gentlemen",
+                "kind regards",
+                "this assay allowed us to measure a wide variety of conditions",
+                "this assay allowed us to measure a wide variety of conditions",
+                "this assay allowed us to measure a wide variety of conditions",
+                "this assay allowed us to measure a wide variety of conditions",
+                "at the home locations there were traces of water",
+                "at the home locations there were traces of water",
+                "at the home locations there were traces of water",
+                "at the home locations there were traces of water",
+                "the development of diabetes is present in mice that carry a transgene",
+                "the development of diabetes is present in mice that carry a transgene",
+                "the development of diabetes is present in mice that carry a transgene",
+                "the development of diabetes is present in mice that carry a transgene",
+            };
             
-            for(String s0: sentences) {
-                System.out.println("Input : " + s0);
-                String result=sc.correctPhrase(s0);
-                System.out.println("Answer: " +result);
-                System.out.println();
+            for(int i = 0; i < sentences.length; ++i) {
+                String original = sentences[i];
+                String correct = correctSentences[i];
+                String result = sc.correctPhrase(original);
+
+                if (! result.equals(correct)) {
+                    System.out.println("Input  : " + original);
+                    System.out.println("Answer : " + result);
+                    System.out.println("Correct: " + correct);
+                    System.out.println();
+                }
             }
     }
     
